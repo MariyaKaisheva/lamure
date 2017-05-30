d498468da2b8567f098a973125a4bfca5c768c98
Mariya Kaisheva <MariyaKaisheva>
2017-05-30 22:33:14 +0200

    adds sorting based on orientaion relative to unit circle

--- apps/points_to_lines_obj_exporter/main.cpp
+++ apps/points_to_lines_obj_exporter/main.cpp
@@ -54,6 +54,37 @@ bool comparator (const xyzall_surfel_t& A, const xyzall_surfel_t& B) {
     return A.y_ > B.y_;
 };
 
+lamure::vec3f compute_average_position_per_layer (std::vector<xyzall_surfel_t> const& surfels_vector) {
+  float average_x = 0.0;
+  float average_y = 0.0;
+  float average_z = 0.0;
+  for(auto const& surfel : surfels_vector){
+    average_x += surfel.x_;
+    average_y += surfel.y_;
+    average_z += surfel.z_;
+  }
+  auto number_of_surfels_per_layer = surfels_vector.size();
+  average_x /= number_of_surfels_per_layer;
+  average_y /= number_of_surfels_per_layer;
+  average_z /= number_of_surfels_per_layer;
+  lamure::vec3f average_position(average_x, average_y, average_z);
+  return average_position;
+}
+
+lamure::vec3f normalize (lamure::vec3f const& centroid_surfel_vec) {
+  auto vector_length = sqrt(centroid_surfel_vec.x*centroid_surfel_vec.x + 
+                            centroid_surfel_vec.y*centroid_surfel_vec.y + 
+                            centroid_surfel_vec.z*centroid_surfel_vec.z);
+  lamure::vec3f result = centroid_surfel_vec / vector_length;
+  return result;
+}
+
+//computes dot product of 2 vectors
+float dot(lamure::vec3f const& vec_A, lamure::vec3f const& vec_B) {
+  float product = vec_A.x*vec_B.x + vec_A.y*vec_B.y + vec_A.z*vec_B.z;
+  return product;
+}
+
 std::vector<line> generate_lines(std::vector<xyzall_surfel_t>& input_data){
     
     //sort input points according to their y-coordinate 
@@ -63,8 +94,6 @@ std::vector<line> generate_lines(std::vector<xyzall_surfel_t>& input_data){
     float threshold = 0.002; //TODO think of alternative for dynamic calculation of thershold value
     std::vector< std::vector <xyzall_surfel_t>> bins; 
     std::vector<xyzall_surfel_t> current_bin_of_surfels(input_data.size()); 
-    //std::cout << "y_min " << input_data.at(0).radius_ << "\n";
-    //std::cout << "y_max " << input_data.at(input_data.size()-1).radius_ << "\n";
     std::vector<line> line_data;
     unsigned long counter = 0; 
     unsigned long offset = floor(input_data.size() / number_line_loops);
@@ -72,18 +101,48 @@ std::vector<line> generate_lines(std::vector<xyzall_surfel_t>& input_data){
         float current_y_min = input_data.at(counter).y_;
         float current_y_max = input_data.at(counter + offset).y_;
         counter += offset;
-        //std::cout << "current_y_min " << current_y_min << "\n";
-        //std::cout << "current_y_max " << current_y_max << "\n";
+
         float current_y_mean = (current_y_min + current_y_max) / 2.0;
-        //std::cout << "current_y_mean " << current_y_mean << "\n\n";
+
         auto copy_lambda = [&]( xyzall_surfel_t const& surfel){return (surfel.y_ >= current_y_mean - threshold) && (surfel.y_ <= current_y_mean + threshold);};
         auto it = std::copy_if(input_data.begin(), input_data.end(), current_bin_of_surfels.begin(), copy_lambda);
         current_bin_of_surfels.resize(std::distance(current_bin_of_surfels.begin(), it));
+        
+    
         for (auto& surfel : current_bin_of_surfels) {
-          surfel.y_ = current_y_mean;
+          surfel.y_ = current_y_mean; //project all surfels for a given y_mean value to a single plane
         }
-        //std::sort(current_bin_of_surfels.begin(), current_bin_of_surfels.end(), [](){});
-       // std::cout << "!current_bin_of_surfels.size() " << current_bin_of_surfels.size() << "\n";
+
+        lamure::vec3f centroid = compute_average_position_per_layer(current_bin_of_surfels);
+
+        auto angle_sorting_lambda = [&](xyzall_surfel_t const& surfel_A,
+                                        xyzall_surfel_t const& surfel_B){
+
+                                              lamure::vec3f surfel_position_A (surfel_A.x_, surfel_A.y_, surfel_A.z_);
+                                              lamure::vec3f surfel_position_B (surfel_B.x_, surfel_B.y_, surfel_B.z_);
+                                              lamure::vec3f centroid_surfel_vec_A = normalize(surfel_position_A - centroid);
+                                              lamure::vec3f centroid_surfel_vec_B = normalize(surfel_position_B - centroid);
+                                              auto plane_rotation_angle_A = dot(centroid_surfel_vec_A, direction_ref_vector);
+                                              auto plane_rotation_angle_B = dot(centroid_surfel_vec_B, direction_ref_vector);
+                                              //both vectors are in the lower half of the unit cirle => 
+                                              //sort in descending order of angle b/n centroid_serfel_vector and reference vector
+                                              if((surfel_position_A.z <= centroid.z) && (surfel_position_B.z <= centroid.z)){
+                                                return plane_rotation_angle_A >= plane_rotation_angle_B; 
+                                              }
+                                              //both vectors are in the upper half of the unit cirle => 
+                                              //sort in ascending order of angle b/n centroid_serfel_vector and reference vector
+                                              else if((surfel_position_A.z >= centroid.z) && (surfel_position_B.z >= centroid.z)) {
+
+                                                return plane_rotation_angle_A <= plane_rotation_angle_B;
+                                              } 
+                                              //vectors are in opposite halfs of the unit cirle => 
+                                              //sort in descending order of z coordinate
+                                              else {
+                                                return surfel_position_A.z <= surfel_position_B.z;
+                                              }
+                                        };
+
+        std::sort(current_bin_of_surfels.begin(), current_bin_of_surfels.end(), angle_sorting_lambda);
         line current_line; 
         if(current_bin_of_surfels.size() > 2) {
           for (uint j = 0; j < (current_bin_of_surfels.size()) - 1; ++j){ 
@@ -187,10 +246,10 @@ int main(int argc, char *argv[]) {
         std::cout << "ok \n";
         for (uint i = 0; i < line_data.size(); ++i){
         //while(i < line_data.size()){
-         output_file << "v " <<  line_data.at(i).start.x_ << " " << line_data.at(i).start.y_ << " " << line_data.at(i).start.z_ << "\n";
-         output_file << "v " <<  line_data.at(i).end.x_ << " " << line_data.at(i).end.y_ << " " << line_data.at(i).end.z_ << "\n";
+         output_file << "v " << std::setprecision(DEFAULT_PRECISION) <<  line_data.at(i).start.x_ << " " << std::setprecision(DEFAULT_PRECISION) << line_data.at(i).start.y_ << " " << std::setprecision(DEFAULT_PRECISION)<< line_data.at(i).start.z_ << "\n";
+         output_file << "v " << std::setprecision(DEFAULT_PRECISION) <<  line_data.at(i).end.x_ << " " << std::setprecision(DEFAULT_PRECISION) << line_data.at(i).end.y_ << " " << std::setprecision(DEFAULT_PRECISION) << line_data.at(i).end.z_ << "\n";
          //vertex duplication to emulate triangle
-         output_file << "v " <<  line_data.at(i).end.x_ << " " << line_data.at(i).end.y_ << " " << line_data.at(i).end.z_ << "\n";
+         output_file << "v " << std::setprecision(DEFAULT_PRECISION) <<  line_data.at(i).end.x_ << " " << std::setprecision(DEFAULT_PRECISION) << line_data.at(i).end.y_ << " " << std::setprecision(DEFAULT_PRECISION) << line_data.at(i).end.z_ << "\n";
          output_file << "f " << vert_counter << " " << (vert_counter + 1) << " " << (vert_counter + 2) << "\n";
          vert_counter += 3;
          //i += 250;
