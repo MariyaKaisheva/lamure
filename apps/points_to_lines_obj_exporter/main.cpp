ceed68ab14b5621c5b1c35627bf3f04984cd923d
Mariya Kaisheva <MariyaKaisheva>
2017-05-30 19:59:07 +0200

    creates descrete layes along y

--- apps/points_to_lines_obj_exporter/main.cpp
+++ apps/points_to_lines_obj_exporter/main.cpp
@@ -11,13 +11,16 @@
 #include <sstream>
 #include <vector>
 #include <algorithm>
+#include <cmath>
+
+
 #include <lamure/ren/model_database.h>
 #include <lamure/bounding_box.h>
-
 #include <lamure/types.h>
 #include <lamure/ren/dataset.h>
 #include <lamure/ren/bvh.h>
 #include <lamure/ren/lod_stream.h>
+
 
 #define DEFAULT_PRECISION 15
 
@@ -46,25 +49,49 @@ struct line{
     xyzall_surfel_t end;
 };
 
+//sort in descending order based on y coordinate value 
 bool comparator (const xyzall_surfel_t& A, const xyzall_surfel_t& B) {
-    return A.y_ < B.y_;
+    return A.y_ > B.y_;
 };
 
 std::vector<line> generate_lines(std::vector<xyzall_surfel_t>& input_data){
     
     //sort input points according to their y-coordinate 
     std::sort(input_data.begin(), input_data.end(), comparator);
-    line current_line; 
-    std::vector<line> line_data;   
-    for (uint i = 0; i < (input_data.size()); ++i){ 
-        if(i < input_data.size()-1){ //TODO think of cleaner solution 
-            current_line.start = input_data.at(i);
-            current_line.end = input_data.at(i+1);
-            line_data.push_back(current_line);  
-        } 
-        else{
-            continue; 
-        }   
+    lamure::vec3f direction_ref_vector (1.0, 0.0, 0.0);
+    unsigned long number_line_loops = 20; //TODO make number dependent on model size
+    float threshold = 0.002; //TODO think of alternative for dynamic calculation of thershold value
+    std::vector< std::vector <xyzall_surfel_t>> bins; 
+    std::vector<xyzall_surfel_t> current_bin_of_surfels(input_data.size()); 
+    //std::cout << "y_min " << input_data.at(0).radius_ << "\n";
+    //std::cout << "y_max " << input_data.at(input_data.size()-1).radius_ << "\n";
+    std::vector<line> line_data;
+    unsigned long counter = 0; 
+    unsigned long offset = floor(input_data.size() / number_line_loops);
+    for(uint i = 0; i < number_line_loops; ++i) {
+        float current_y_min = input_data.at(counter).y_;
+        float current_y_max = input_data.at(counter + offset).y_;
+        counter += offset;
+        //std::cout << "current_y_min " << current_y_min << "\n";
+        //std::cout << "current_y_max " << current_y_max << "\n";
+        float current_y_mean = (current_y_min + current_y_max) / 2.0;
+        //std::cout << "current_y_mean " << current_y_mean << "\n\n";
+        auto copy_lambda = [&]( xyzall_surfel_t const& surfel){return (surfel.y_ >= current_y_mean - threshold) && (surfel.y_ <= current_y_mean + threshold);};
+        auto it = std::copy_if(input_data.begin(), input_data.end(), current_bin_of_surfels.begin(), copy_lambda);
+        current_bin_of_surfels.resize(std::distance(current_bin_of_surfels.begin(), it));
+        for (auto& surfel : current_bin_of_surfels) {
+          surfel.y_ = current_y_mean;
+        }
+        //std::sort(current_bin_of_surfels.begin(), current_bin_of_surfels.end(), [](){});
+       // std::cout << "!current_bin_of_surfels.size() " << current_bin_of_surfels.size() << "\n";
+        line current_line; 
+        if(current_bin_of_surfels.size() > 2) {
+          for (uint j = 0; j < (current_bin_of_surfels.size()) - 1; ++j){ 
+            current_line.start = current_bin_of_surfels.at(j);
+            current_line.end = current_bin_of_surfels.at(j+1);
+            line_data.push_back(current_line);
+          }
+        }
     }   
     
     return line_data;   
@@ -117,7 +144,7 @@ int main(int argc, char *argv[]) {
     out_stream.open(obj_filename, std::ios::out | std::ios::trunc); // Any current content is discarded
     out_stream.close();
 
-    uint64_t num_surfels_excluded = 0;
+    //uint64_t num_surfels_excluded = 0;
 
     auto total_num_surfels = num_leafs*bvh->get_primitives_per_node();
     std::vector<xyzall_surfel_t> surfels_vector(total_num_surfels);
