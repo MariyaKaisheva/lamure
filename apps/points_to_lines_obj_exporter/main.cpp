904db402de6b1799b7471c1c078feab5e5544285
Mariya Kaisheva <MariyaKaisheva>
2017-07-19 22:22:37 +0200

    move clustering functions into separate file

--- apps/points_to_lines_obj_exporter/main.cpp
+++ apps/points_to_lines_obj_exporter/main.cpp
@@ -22,6 +22,7 @@
 #include <lamure/ren/bvh.h>
 #include <lamure/ren/lod_stream.h>
 
+#include "clustering.h"
 #include "color_hash_map.hpp"
 #include "math_wrapper.h"
 #include "nurbscurve.hpp"
@@ -44,44 +45,6 @@ bool cmd_option_exists(char** begin, char** end, const std::string& option) {
     return std::find(begin, end, option) != end;
 }
 
-//surfel layout as it is used for rendering 
-struct xyzall_surfel_t {
-  xyzall_surfel_t() : radius_(0.0) {}
-  float pos_coordinates[3];
-  uint8_t r_, g_, b_, fake_;
-  float radius_;
-  float nx_, ny_, nz_;
-};
-
-struct point{
-point() : pos_coordinates_{0.0, 0.0, 0.0}, id_(0), r_(0), g_(150), b_(50), is_used_(false), is_member_of_cluster_(false) {}
-point(float* pos) : pos_coordinates_{pos[0], pos[1], pos[2]},  id_(0), r_(200), g_(150), b_(50), is_used_(false), is_member_of_cluster_(false) {}                        
-point(float* pos, int32_t id) : pos_coordinates_{pos[0], pos[1], pos[2]},  id_(id), r_(200), g_(150), b_(50), is_used_(false), is_member_of_cluster_(false) {}   
-point(float* pos, int32_t id, uint8_t red, uint8_t green, uint8_t blue, bool is_used = false, bool is_member_of_cluster = false) : 
-                          pos_coordinates_{pos[0], pos[1], pos[2]},
-                          id_(id),
-                          r_(red),
-                          g_(green),
-                          b_(blue),
-                          is_used_(is_used),
-                          is_member_of_cluster_(is_member_of_cluster) {}
-
-bool operator==(point const& rhs){
-  return (*this).id_ == rhs.id_;
-}
-
-void set_color(lamure::vec3b const& color_vector) {
-  r_ = color_vector[0];
-  g_ = color_vector[1];
-  b_ = color_vector[2];
-}
-
-float pos_coordinates_[3];
-int32_t id_;
-uint8_t r_, g_, b_;
-bool is_used_;
-bool is_member_of_cluster_;
-};
 
 struct line{
     point start;
@@ -89,8 +52,7 @@ struct line{
     float length;
 };
 
-using bins_t = std::vector<xyzall_surfel_t>;
-using clusters_t = std::vector<point>;
+
 //using line_data_t = std::vector<line>;
 
 //sort in descending order based on y coordinate value 
@@ -130,14 +92,6 @@ float dot(lamure::vec3f const& vec_A, lamure::vec3f const& vec_B) {
   return product;
 }
 
-float compute_distance(lamure::vec3f const& pos1, lamure::vec3f const& pos2) {
-
-  lamure::vec3f distance_vector((pos1.x - pos2.x), (pos1.y - pos2.y), (pos1.z - pos2.z));
-  float result = sqrt(distance_vector.x*distance_vector.x +
-                      distance_vector.y*distance_vector.y +
-                      distance_vector.z*distance_vector.z);
-  return result;
-}
 
 float compute_global_average_line_length(std::vector<line> const& all_lines) {
   float avg_line_length = 0.0; 
@@ -153,186 +107,8 @@ float compute_global_average_line_length(std::vector<line> const& all_lines) {
   }
 }
 
-std::pair<float, point> find_nearest_neighbour (point const& start_point, std::vector<point> const& all_points) {
-  float current_min = std::numeric_limits<float>::max();
-  point current_nearest_neighbour;
-
-  bool is_valid = false;
-
-  for (auto& p : all_points){
-    if((!p.is_used_)){
-      float distance = compute_distance(lamure::vec3f(p.pos_coordinates_[0], p.pos_coordinates_[1], p.pos_coordinates_[2]), 
-                                        lamure::vec3f(start_point.pos_coordinates_[0], start_point.pos_coordinates_[1], start_point.pos_coordinates_[2]));
-      if( distance > 0.0 && distance <= current_min ){
-        current_min = distance;
-        current_nearest_neighbour = p;
-
-        is_valid = true;
-      }
-    }
-  }
-
-  if( !is_valid ) {
-    current_min = std::numeric_limits<float>::max();
-  }
-
-  std::pair<float, point> result(current_min, current_nearest_neighbour);
-  return result;
-}
-
-
-std::vector<point*> find_near_neighbours(float max_distance, point const& start_point, std::vector<point>& all_points){
-  std::vector<point*> neighbourhood;
-  for(auto& p : all_points){
-
-    float distance = compute_distance(lamure::vec3f(p.pos_coordinates_[0], p.pos_coordinates_[1], p.pos_coordinates_[2]), 
-                                      lamure::vec3f(start_point.pos_coordinates_[0], start_point.pos_coordinates_[1], start_point.pos_coordinates_[2]));
-    if(distance <= max_distance ){
-      neighbourhood.push_back(&p);
-    }
-  }
-  
-  return neighbourhood; 
-}
-
-std::vector<clusters_t> create_clusters (bins_t& all_surfels_per_layer){
-   
-  //create vector of points, containg all surfels per layer in point format
-  std::vector<point> all_points_per_layer;
-  int32_t id_counter = 0; 
-  for (auto& s: all_surfels_per_layer){
-    point current_point(s.pos_coordinates, id_counter, s.r_, s.g_, s.b_, false);
-    all_points_per_layer.push_back(current_point);
-    ++id_counter;
-  }
-
-  //auto centroid = compute_average_position_per_layer(all_surfels_per_layer);
-  std::vector<point> point_cluster;
-  std::vector<clusters_t> clusters_vector;
-  float distance_threshold = 60.0; 
-  
-  float last_measured_distance = 0;  //TODO think about this value
-
-  //int cluster_counter  = 0; 
-
-  //distribute points to clusters based on distance to closest available neigbour point
-  for(uint point_iterator = 0; point_iterator < all_points_per_layer.size(); ++point_iterator){
-      int point_index = point_iterator;
-     // bool next_point_is_used = false;
-      do {
-        point* current_point = &all_points_per_layer[point_index];
-        if(!current_point->is_used_){
-            point_cluster.push_back(*current_point);
-            current_point->is_used_ = true;
-
-            //reset distance record for each new cluster; no distance has been stored so far
-            if(point_cluster.size() < 2){
-              last_measured_distance = std::numeric_limits<float>::max() / (2.0 * distance_threshold); 
-            }
-            
-            auto search_result =  find_nearest_neighbour(*current_point, all_points_per_layer);
-            auto distance_to_nearest_unused_point = search_result.first; 
-            auto next_point = search_result.second; //function find_nearest_neighbour() makes sure next point is not used so far
-            
-            
-            if(distance_to_nearest_unused_point <= (last_measured_distance * distance_threshold)){  
-              point_cluster.push_back(next_point);
-             // next_point.is_used_ = true;
-              last_measured_distance  = distance_to_nearest_unused_point; 
-              point_index = next_point.id_;
-            }
-            else{
-             // next_point.is_used_ = false;
-              //push current cluster to common container and start a new
-              clusters_vector.push_back(point_cluster);
-             // ++cluster_counter; 
-              point_cluster.clear();
-            }
-            //next_point_is_used = next_point.is_used_;
-        } else {
-          break;
-        }
-      } while(true); //!next_point_is_used is always true 
-  }  
-
-  //std::cout << cluster_counter << std::endl;
-  return clusters_vector; 
-}
-
-
-void expand_cluster(point const& current_point, std::vector<point*>& neighbourhood, std::vector<point>& all_points_per_layer, clusters_t& current_cluster, float eps, uint8_t min_points) {
-  current_cluster.push_back(current_point);
-
-  auto size = neighbourhood.size();
-
-  for(uint32_t point_itr = 0; point_itr < size; ++point_itr) {
-    auto& neighbour_point = neighbourhood.at(point_itr);  
-    if(!neighbour_point->is_used_){ //if P' is not visited
-      neighbour_point->is_used_ = true; //mark P' as visited
-      auto next_neighbourhood = find_near_neighbours(eps, *neighbour_point, all_points_per_layer); //region Query (P', eps)
-      if(next_neighbourhood.size() >= min_points){ 
-        for(auto& next_neighbour_point : next_neighbourhood){
-          if(!next_neighbour_point->is_used_) {
-            if( std::find(neighbourhood.begin(), neighbourhood.end(), next_neighbour_point) == neighbourhood.end() ){
-              neighbourhood.push_back(next_neighbour_point);
-            }
-          }
-        }
-
-      }
-      size = neighbourhood.size();
-
-      if( !neighbour_point->is_member_of_cluster_ ) {
-        neighbour_point->is_member_of_cluster_ = true;
-        current_cluster.push_back(*neighbour_point);
-      }
-    }
-  }
-}
 
 uint32_t current_cluster_id = 0;
-
-std::vector<clusters_t> create_DBSCAN_clusters (bins_t& all_surfels_per_layer, float eps, uint8_t min_points){
-  //create vector of points, containg all surfels per layer in point format
-  std::vector<point> all_points_per_layer;
-  uint32_t id_counter = 0; 
-  for (auto& s: all_surfels_per_layer){
-    point current_point(s.pos_coordinates, id_counter, s.r_, s.g_, s.b_);
-    all_points_per_layer.push_back(current_point);
-    ++id_counter;
-  }
-
-  std::vector<clusters_t> clusters_vec;
-
-
-  //uint32_t current_cluster_id = 0;
-
-  //iterate over all points 
-  for (auto& current_point : all_points_per_layer) {
-    if(current_point.is_used_){
-      continue; //point has been already evaluated
-    } 
-    else{
-      current_point.is_used_ = true;
-      auto neighbourhood = find_near_neighbours(eps, current_point, all_points_per_layer);
-
-      neighbourhood.reserve(all_surfels_per_layer.size());
-
-      if(neighbourhood.size() < min_points){
-        continue; //point is considered noise and is not added to any cluster
-      }
-      else{
-        clusters_t current_cluster;
-        expand_cluster(current_point, neighbourhood, all_points_per_layer, current_cluster, eps, min_points);
-
-        clusters_vec.push_back(current_cluster);
-
-      }
-    }
-  }
-
-  return clusters_vec;
-}
 
 //using namespace gpucast::math;
 std::vector<line> generate_lines_from_curve (std::vector<point> const& cluster_of_points) {
@@ -402,7 +178,7 @@ std::vector<line> generate_lines_from_curve (std::vector<point> const& cluster_o
     line current_line;
     current_line.start = current_start_point;
     current_line.end = current_end_point;
-    current_line.length = compute_distance(lamure::vec3f(current_line.start.pos_coordinates_[0], current_line.start.pos_coordinates_[1], current_line.start.pos_coordinates_[2]),
+    current_line.length = clustering::compute_distance(lamure::vec3f(current_line.start.pos_coordinates_[0], current_line.start.pos_coordinates_[1], current_line.start.pos_coordinates_[2]),
                                            lamure::vec3f(current_line.end.pos_coordinates_[0], current_line.end.pos_coordinates_[1], current_line.end.pos_coordinates_[2])); 
     line_segments_vec.push_back(current_line);
     //parameter_t += sampling_step;
@@ -451,12 +227,12 @@ std::vector<line> generate_lines(std::vector<xyzall_surfel_t>& input_data, unsig
 
         std::vector<clusters_t> all_clusters_per_bin_vector;
         if(apply_naive_clustering){
-          all_clusters_per_bin_vector = create_clusters(current_bin_of_surfels);
+          all_clusters_per_bin_vector = clustering::create_clusters(current_bin_of_surfels);
         }else{
           float eps = 0.005;
           uint8_t minPots = 3;
           //std::cout << "XX_C_XX STARTING DB SCAN WITH " << current_bin_of_surfels.size() << " surfels\n";
-          all_clusters_per_bin_vector = create_DBSCAN_clusters(current_bin_of_surfels, eps, minPots);
+          all_clusters_per_bin_vector = clustering::create_DBSCAN_clusters(current_bin_of_surfels, eps, minPots);
         }
 
         //color clusters
@@ -472,10 +248,9 @@ std::vector<line> generate_lines(std::vector<xyzall_surfel_t>& input_data, unsig
         std::cout << "all_clusters_per_bin_vector.size(): " <<  all_clusters_per_bin_vector.size() << " \n";
         line current_line; 
 
-        /*-std::cout << "XX_C_XX: Num Points per Cluster:\n";
-        for(auto const& points_per_cluster : all_clusters_per_bin_vector) {
-          std::cout << "XX_C_XX: " << points_per_cluster.size() << "\n";
-        } */
+        //sampling step
+
+
 
         if(all_clusters_per_bin_vector.size() > 0){
 
@@ -525,7 +300,7 @@ std::vector<line> generate_lines(std::vector<xyzall_surfel_t>& input_data, unsig
                 for (uint line_idx = 0; line_idx < num_lines_to_push; ++line_idx) { 
                   current_line.start = current_cluster.at(line_idx);
                   current_line.end = current_cluster.at(line_idx+1);
-                  current_line.length = compute_distance(lamure::vec3f(current_line.start.pos_coordinates_[0], current_line.start.pos_coordinates_[1], current_line.start.pos_coordinates_[2]),
+                  current_line.length = clustering::compute_distance(lamure::vec3f(current_line.start.pos_coordinates_[0], current_line.start.pos_coordinates_[1], current_line.start.pos_coordinates_[2]),
                                                           lamure::vec3f(current_line.end.pos_coordinates_[0], current_line.end.pos_coordinates_[1], current_line.end.pos_coordinates_[2]));
                   line_data.push_back(current_line);
                 }
