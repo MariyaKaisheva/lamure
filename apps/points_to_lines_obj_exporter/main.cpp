4f978ad2f8ebbeb1d77cd08a8a75ce307610eb22
Mariya Kaisheva <MariyaKaisheva>
2017-07-20 16:35:07 +0200

    implement first version of random cluster sampling; add forgotten clustering files

--- apps/points_to_lines_obj_exporter/main.cpp
+++ apps/points_to_lines_obj_exporter/main.cpp
@@ -14,6 +14,7 @@
 #include <algorithm>
 #include <memory>
 #include <limits>
+#include <random>
 
 #include <lamure/ren/model_database.h>
 #include <lamure/bounding_box.h>
@@ -248,9 +249,6 @@ std::vector<line> generate_lines(std::vector<xyzall_surfel_t>& input_data, unsig
         std::cout << "all_clusters_per_bin_vector.size(): " <<  all_clusters_per_bin_vector.size() << " \n";
         line current_line; 
 
-        //sampling step
-
-
 
         if(all_clusters_per_bin_vector.size() > 0){
 
@@ -292,7 +290,56 @@ std::vector<line> generate_lines(std::vector<xyzall_surfel_t>& input_data, unsig
 
               }*/
 
+              //sampling step (reduction of points to chosen % of original amout of points per cluster)
 
+              float const sampling_rate = 0.5; //persentage points to remain after sampling
+
+              float min_x = std::numeric_limits<float>::max();
+              float max_x = std::numeric_limits<float>::lowest();
+              float min_z = std::numeric_limits<float>::max();
+              float max_z = std::numeric_limits<float>::lowest();
+              for (auto & current_point : current_cluster){
+                min_x = std::min(min_x, current_point.pos_coordinates_[0]);
+                max_x = std::max(max_x, current_point.pos_coordinates_[0]);
+                min_z = std::min(min_z, current_point.pos_coordinates_[2]);
+                max_z = std::max(max_z, current_point.pos_coordinates_[2]);
+              }
+
+              //
+              std::random_device rd;
+              std::mt19937 g(rd());
+              
+       
+              auto num_cells_x_direction = 10;
+              auto num_cells_z_direction = 10;
+              auto cell_length = (max_z - min_z) / num_cells_z_direction;
+              auto cell_width = (max_x - min_x) / num_cells_x_direction;
+
+              std::vector<std::vector<point*>> vector_of_cells (num_cells_x_direction * num_cells_z_direction);
+
+              //split all cluter points into respective grid cells
+              for (auto & current_point : current_cluster){ //TODO check constness
+                auto x_index = std::min(num_cells_x_direction - 1, std::max(0, int( (current_point.pos_coordinates_[0] - min_x) / cell_width)) );
+                auto z_index = std::min(num_cells_z_direction - 1, std::max(0, int( (current_point.pos_coordinates_[2]- min_z) / cell_length) ) );
+                std::cout<< " Index X: " << x_index << " Index Z: " << z_index << "\n";
+                int64_t cell_index = z_index * num_cells_x_direction + x_index;
+                auto& current_cell = vector_of_cells[cell_index];
+                current_cell.push_back(&current_point);
+              }
+              auto num_points_in_cluster = current_cluster.size();
+              int total_num_remaining_points = num_points_in_cluster * sampling_rate;
+              std::vector<point> sampled_cluster;
+              for (auto& current_cell : vector_of_cells) {
+                std::shuffle(current_cell.begin(), current_cell.end(), g);
+                uint64_t num_point_to_remain_in_cell = current_cell.size()*sampling_rate;
+                current_cell.resize(num_point_to_remain_in_cell);
+                current_cell.shrink_to_fit(); 
+                for(auto& current_point : current_cell){
+                  sampled_cluster.push_back(*current_point);
+                }
+              }
+
+              std::cout << sampled_cluster.size() << " vs " << total_num_remaining_points << "\n";
               if(!use_nurbs){ 
 
                 uint32_t num_lines_to_push = (current_cluster.size()) - 1;
